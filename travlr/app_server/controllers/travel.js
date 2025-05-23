05d91173355871991f3f4155f0095d2fa3de6dbc
Jeba Singh Emmanuel <jebasingh.emmanuel@gmail.com>
2025-05-22 21:12:25 -0700

    Module 3 completed baseline

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,7 +1,9 @@
 /* GET Travel page */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips: trips });
 };
 
 module.exports = {
